64e930982adc3bfe7e35366839d2b80301c23ff9
fierylion <mawalladaniel2018@gmail.com>
2023-04-15 14:03:18 +0300

    user phase 1

--- src/user/App.js
+++ src/user/App.js
@@ -2,12 +2,15 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Register from './pages/Register'
 import Login from './pages/Login'
+import Home from './pages/Home'
 const App = () => {
   return (
      <Routes>
       <Route path='/register' element={<Register/>}/>
       <Route path='/login' element={<Login/>}/>
+      <Route path='/:userName' element={<Home/>}/>
      </Routes>
+     
   
   )
 }
